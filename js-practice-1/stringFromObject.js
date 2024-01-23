7d251b5195720529f4c51b9cda3fc7d4366631e3
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2024-01-23 11:28:30 -0500

    join entries

--- js-practice-1/stringFromObject.js
+++ js-practice-1/stringFromObject.js
@@ -1,4 +1,9 @@
 // add whatever parameters you deem necessary - good luck!
-function stringFromObject(){
-
+function stringFromObject(object){
+    //convert object to array
+    //join with = then with ,
+    return Object.entries(object).map(pair => pair.join(" = ")).join(", ")
 }
+
+console.log(stringFromObject({ a: 1, b: '2' }));
+// "a = 1, b = 2"
